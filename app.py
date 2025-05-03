8eeddd2140c875779082e2406fae236395e21d76
Ryan <mock.ry.34@gmail.com>
2025-05-03 14:14:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def get_events():
     return jsonify(events)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) # Use PORT from environment
+    app.run(host="0.0.0.0", port=port, debug=True)
